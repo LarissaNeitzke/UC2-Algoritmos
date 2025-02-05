4f88fc77b28a7c594c12fcb1fff7336e2e68345b
LarissaNeitzke <larissasouza.neitzke@gmail.com>
2025-02-05 19:24:33 -0300

    Add files via upload

---
+++ 04-02-2025/Exercicio.js
@@ -0,0 +1,17 @@
+const nome = "Larissa"
+const sobrenome = "Neitzke"
+let idade = 28
+let estudante = true
+
+console.log ("Olá! Meu nome é", nome, sobrenome, "tenho", idade, "anos e sou estudante?", estudante)
+
+
+console.log(typeof nome, typeof idade)
+
+const atrizNome = "Fernanda Torres"
+const premio1 = "Globo de Ouro"
+const premio2 = "Premio de Melhor Atriz do Festival de Cinema de Veneza"
+const premio3 = "Premio Shell de Melhor Atriz"
+const objetivo = "Oscar"
+
+console.log("Esta é a atriz", atrizNome, ". Ela já consquistou os seguintes premios:", premio1, premio2, premio3, ". Atualmente está determinada a ganhar o", objetivo)
